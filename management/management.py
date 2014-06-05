5556ad8f3d9d4b0261d46b462168c4006af5d343
Gregor von Laszewski <laszewski@gmail.com>
2014-06-05 09:12:33 -0400

    pseudo code

---
+++ management/management.py
@@ -0,0 +1,42 @@
+class People
+
+    person = {}
+
+    project = {}
+
+    def add_person(p)
+        pass
+
+    def add_project(p)
+        pass
+
+    def add_role(person, project):
+        pass
+
+    def __str__():
+        return ""
+        
+class Person
+
+    def __init__()
+
+
+class Project
+
+    def __init__()
+
+        
+class Role
+
+    def __init__():
+        pass
+
+    def add(role):
+        pass
+    
+    def list():
+        """returns an array of roles
+        
+    
+
+    
