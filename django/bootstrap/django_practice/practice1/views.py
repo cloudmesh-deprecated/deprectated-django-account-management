154ec3ea31a4993e7e5ac0741801950086a8e89c
Jefferson Ridgeway <Jdridgeway4@gmail.com>
2014-06-16 16:05:23 -0400

    Figured out how to make data displayed except in table format yet

--- django/bootstrap/django_practice/practice1/views.py
+++ django/bootstrap/django_practice/practice1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django_tables2   import RequestConfig
 from practice1.models  import Person
+import django_tables2 as tables
 #from practice1.tables  import PersonTable
 #from django.http import HttpResponse
 
@@ -14,9 +15,25 @@ from practice1.models  import Person
 
 	#return HttpResponse(result)
 
+
+
+
+
+data = [
+    {"name": "Bradley"},
+    {"name": "Stevie"},
+]
+
+class NameTable(tables.Table):
+    name = tables.Column()
+
+table = NameTable(data)
+
+
+
 def people(request):
-	table = PersonTable(Person.objects.all())
-    	RequestConfig(request).configure(table)
+	#table = PersonTable(Person.objects.all())
+    	#RequestConfig(request).configure(table)
    	return render(request, 'people.html', {'table': table})
 
 
