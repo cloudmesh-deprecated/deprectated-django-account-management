3743b196cc16d47449da78de922470efc836e506
Jefferson Ridgeway <Jdridgeway4@gmail.com>
2014-06-19 13:46:08 -0400

    Figured out how to create a template and use the view to display the template with made up data

--- django/bootstrap/django_practice/practice1/views.py
+++ django/bootstrap/django_practice/practice1/views.py
@@ -1,39 +1,73 @@
-from django.shortcuts import render, render_to_response, RequestContext
+from django.template.loader import get_template
+from django.template import Context
+#from django.shortcuts import render, render_to_response, RequestContext
 #from django_tables2   import RequestConfig
 #from practice1.models  import Person
 #import django_tables2 as tables
 #from practice1.tables  import PersonTable
 from django.http import HttpResponse
+import datetime
+
+
+def current_datetime(request):
+	now = datetime.datetime.now()
+	t = get_template('practicetemplate.html')
+	html = t.render(Context({'current_date':now}))
+	return HttpResponse(html)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Create your views here.
 
-def index(request):
+#def index(request):
 
-	html = """<!DOCTYPE html>
+	#html = """<!DOCTYPE html>
 
-<html>
-	<head>
-	</head>
+#<html>
+	#<head>
+	#</head>
 	
-	<body>
-		<table style="width:300px">
-		<tr>
-			<td> Jeff</td>
-			<td> Ridgeway</td>
-			<td> 19 </td>
-		</tr>
-		<tr>
-			<td> Marian </td>
-			<td> Ridgeway </td>
-			<td> 50 </td>
-		</tr>
-		</table>
-	</body>
-</html>"""
+	#<body>
+		#<table style="width:300px">
+		#<tr>
+			#<td> Jeff</td>
+			#<td> Ridgeway</td>
+			#<td> 19 </td>
+		#</tr>
+		#<tr>
+			#<td> Marian </td>
+			#<td> Ridgeway </td>
+			#<td> 50 </td>
+		#</tr>
+		#</table>
+	#</body>
+#</html>"""
 	#result = str(index.html) 
 
-	return HttpResponse(html)
+	#return HttpResponse(html)
 
 #def example1(request):
 
