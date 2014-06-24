5953be074b1e849d1421efa2da9d4446077d0e90
ifeanyi <rowlandifeanyi17@gmail.com>
2014-06-24 17:49:11 -0400

    Updated the program with functions

--- management/projects.py
+++ management/projects.py
@@ -25,6 +25,28 @@ class project(Document):
     slide_collection_aggreement = StringField()	#Yes/No
     other = StringField()
     
+    def to_json(self):
+        u = {"information":self.information, 
+             "requirements":self.requirements,
+             "project_title":self.project_title, 
+             "category":self.category, 
+             "keywords":self.keywords, 
+             "lead":self.lead, 
+             "manager":self.manager, 
+             "contact":self.contact,
+             "members":self.members, 
+             "alumni":self.alumni,
+             "nsf_grant_number":self.nsf_grant_number, 
+             "nsf_grant_url":self.nsf_grant_url, 
+             "results":self.results,
+             "nsf_Aggreement":self.nsf_Aggreement, 
+             "slide_collection_aggreement":self.slide_collection_aggreement,
+             "other":self.other}
+             
+    def __str__(self):
+        u = self.to_json()
+        return str(u)
+    
 class project_information(Document):
     orientation = StringField() 		#***
     primary_discipline = StringField()		#***
@@ -35,7 +57,11 @@ class project_information(Document):
     documentation_contribution = StringField()	#Yes/No
     support_Software = StringField()		#Yes/No
     
-    def 
+    def __str__(self):
+        return "{0} {1} {2} {3} {4} {5} {6} {7}".format(self.orientation, self.primary_discipline,
+        						self.abstract, self.intellectual_merit,
+        						self.broader_impact, self.software_contribution,
+        						self.documentation_contribution, self.support_Software)
     
 class resource_requirement(Document):    
     hardware_resources = ListField(StringField())
@@ -44,4 +70,10 @@ class resource_requirement(Document):
     server = ListField(StringField())
     comment = StringField()
     use_of_fg = StringField()
-    scale of use = StringField()
+    scale_of_use = StringField()
+    
+    def __str__(self):
+        return "{0} {1} {2} {3} {4} {5} {6}".format(self.hardware_resources, self.provision_type,
+        					    self.base_environment, self.server,
+        					    self.comment, self.use_of_fg,
+        					    self.scale_of_use)
