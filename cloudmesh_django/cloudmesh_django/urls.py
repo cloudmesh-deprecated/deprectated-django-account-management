2fac1f9f154a17392f4a0f1b8a0c488598af9a49
Gregor von Laszewski <laszewski@gmail.com>
2014-07-20 08:18:57 -0400

    added placeholders for pages to be implemented

--- cloudmesh_django/cloudmesh_django/urls.py
+++ cloudmesh_django/cloudmesh_django/urls.py
@@ -14,7 +14,17 @@ urlpatterns = patterns('',
     url(r'^$', 'cloudmesh_app.views.index', name = 'index'),
 
     url(r'^list/', 'cloudmesh_app.views.list', name = 'list'),
+
+    url(r'^user/apply/', 'cloudmesh_app.views.user_apply', name = 'user_apply'),
+    url(r'^user/approve/', 'cloudmesh_app.views.user_approve', name = 'user_approve'),
+    url(r'^user/list/', 'cloudmesh_app.views.user_list', name = 'user_list'),
+    url(r'^user/manage/', 'cloudmesh_app.views.user_manage', name = 'user_manage'),                   
+    
     url(r'^project/apply/', 'cloudmesh_app.views.project_apply', name = 'project_apply'),
-    url(r'^user/apply/', 'cloudmesh_app.views.user_apply', name = 'user_apply'),           
+    url(r'^project/approve/', 'cloudmesh_app.views.project_approve', name = 'project_approve'),
+    url(r'^project/list/', 'cloudmesh_app.views.project_list', name = 'project_list'),
+    url(r'^project/manage/', 'cloudmesh_app.views.project_manage', name = 'project_manage'),
+    url(r'^project/results/', 'cloudmesh_app.views.project_result', name = 'project_results'),
+    url(r'^project/members/', 'cloudmesh_app.views.project_members', name = 'project_members'),                           
     
 )
