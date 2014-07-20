9038b743ba9376ba237dc6806b9765607d0ee02a
Gregor von Laszewski <laszewski@gmail.com>
2014-07-20 08:20:58 -0400

    fixing indentation

--- cloudmesh_django/cloudmesh_app/views.py
+++ cloudmesh_django/cloudmesh_app/views.py
@@ -1,50 +1,50 @@
 from django.shortcuts import render
 
 def index(request):
-        return render(request, 'index.html',)
+    return render(request, 'index.html',)
 
 def list(request):
-        return render(request, 'list.html',)
+    return render(request, 'list.html',)
 
 def project_apply(request):
-        return render(request, 'project_apply.html',)
+    return render(request, 'project_apply.html',)
 
 #
 # USERS
 #
 
 def user_apply(request):
-        return render(request, 'user_apply.html',)
+    return render(request, 'user_apply.html',)
 
 def user_list(request):
-        return render(request, 'user_list.html',)
+    return render(request, 'user_list.html',)
 
 def user_approve(request):
-        return render(request, 'user_approve.html',)
+    return render(request, 'user_approve.html',)
 
 def user_manage(request):
-        return render(request, 'user_manage.html',)
+    return render(request, 'user_manage.html',)
 
 #
 # PROJECTS
 #
 
 def project_apply(request):
-        return render(request, 'project_apply.html',)
+    return render(request, 'project_apply.html',)
 
 def project_list(request):
-        return render(request, 'project_list.html',)
+    return render(request, 'project_list.html',)
 
 def project_approve(request):
-        return render(request, 'project_approve.html',)
+    return render(request, 'project_approve.html',)
 
 def project_result(request):
-        return render(request, 'project_result.html',)
+    return render(request, 'project_result.html',)
 
 def project_members(request):
-        return render(request, 'project_members.html',)
+    return render(request, 'project_members.html',)
 
 def project_manage(request):
-        return render(request, 'project_manage.html',)
+    return render(request, 'project_manage.html',)
 
     
