eea22fe0fce277d1a752accdbd23a98c5ad53457
Gregor von Laszewski <laszewski@gmail.com>
2014-07-20 03:23:45 -0400

    fixing random project generator

--- management/generate.py
+++ management/generate.py
@@ -40,57 +40,85 @@ def random_user():
     return data
 
 
-print 70 * "="
-users.clear()
-for i in range(0,10):
-    data = random_user()
-    print data
-    users.add(data)    
+def generate_users(n):
+    users.clear()
+    for i in range(0,n):
+        data = random_user()
+        print data
+        users.add(data)    
 
-print 70 * "="
-print users.find()
 
-sys.exit()
 
+def random_project():
+    firstname = fake.first_name()    
+    data = User(
+        title = fake.prefix(),
+        firstname = firstname,
+        lastname = fake.last_name(),
+        email = fake.safe_email(),
+        username = firstname.lower(),
+        active = True,
+        password = fake.word(),
+        phone = fake.phone_number(),
+        department = "IT",
+        institution = fake.company(),
+        address = fake.address(),
+        country = "USA",
+        citizenship = "US",
+        bio = fake.paragraph(),
+    )
+    return data
+
+
+
+        
       
 
 
-def add_projects():
+def random_project():
     """This function adds projects"""
-    projects.clear()
-    django = Project(
+    data = Project(
     	    title = "Django",
-    	    abstract = "This is my abstract",
-            intellectual_merit = "All about the merit thingy",    
-            broader_impact = "Everything is broad according ...",
-            use_of_fg = "Fg is about to use it",
-            scale_of_use = "Would be used to implement djandgo",
+    	    abstract = fake.paragraph(),
+            intellectual_merit = fake.paragraph(),
+            broader_impact = fake.paragraph(),
+            use_of_fg = fake.paragraph(),
+            scale_of_use = fake.paragraph(),
             categories = ['FutureGrid'],
             keywords = ['sqllite'],
             primary_discipline = "other",  
             orientation = "Lot's of all make",
-            contact = "Bloomington",
-            url = 'https://www.facebook.com/',
+            contact = fake.name() +"\n" + fake.address(),
+            url = fake.url(),
             active = True,
             status = "pending",
-            username = "gregvon",
             resources_services = ['hadoop','openstack'],
             resources_software = ['other'],
             resources_clusters = ['india'],
             resources_provision = ['paas']
           )
-    print django.abstract
-    projects.add_project(django)
+    return data
+    #projects.add_member("gregvon1", data)
     
-    projects.add_member("gregvon1", django)
-    
-    projects.find()
 
+
+def generate_projects(n):
+    projects.clear()
+    for i in range(0,n):
+        data = random_project()
+        print data
+        projects.add(data)    
+    
 
 def main():
 
-    add_users()
-    add_projects()
+    generate_users(10)
+    generate_projects(3)    
+
+    print 70 * "="
+    print users.find()
+    print 70 * "="    
+    print projects.find()
 
     
 
