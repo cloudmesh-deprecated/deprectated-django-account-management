9cd9fbf5e380b9d2316ad6419eb72a3b9197f407
Jefferson Ridgeway <Jdridgeway4@gmail.com>
2014-07-21 13:09:13 -0400

    changed form and added model

--- cloudmesh_django/cloudmesh_app/forms.py
+++ cloudmesh_django/cloudmesh_app/forms.py
@@ -1,11 +1,20 @@
 # forms.py
 from django import forms
+from models import Message
+from cloudmesh_management.user import User, Users
+
 
 class ContactForm(forms.Form):
     name = forms.CharField()
     message = forms.CharField(widget=forms.Textarea)
 
     def handle_success(self):
+    	message = Message.objects.create(
+    		name =  self.cleaned_data['name'],
+    		message = self.cleaned_data['message']
+    	)
+    	message.save()
         # send email using the self.cleaned_data dictionary
         print self.cleaned_data
         pass
+
